6137c3299f7183445660c8a7f03e3210761ed47a
Chris <chris@starforge.co.uk>
2012-01-23 17:25:00 +0000

    Added mootools datepicker class to assist dates/year selection in UI.

---
+++ templates/default/datepicker/Locale.en-GB.DatePicker.js
@@ -0,0 +1,19 @@
+/*
+---
+name: Locale.en-GB.DatePicker
+description: English Language File for DatePicker
+authors: Chris Page
+requires: [More/Locale]
+provides: Locale.en-US.DatePicker
+...
+*/
+
+
+Locale.define('en-GB', 'DatePicker', {
+	select_a_time: 'Select a time',
+	use_mouse_wheel: 'Use the mouse wheel to quickly change value',
+	time_confirm_button: 'OK',
+	apply_range: 'Apply',
+	cancel: 'Cancel',
+	week: 'Wk'
+});
