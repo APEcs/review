235b2782de838b661b6c374c77cab35e6b50199e
Chris <chris@starforge.co.uk>
2012-02-09 10:36:37 +0000

    Fixed problems with summary ID in javascript.

--- templates/default/summary/summary.js
+++ templates/default/summary/summary.js
@@ -8,15 +8,4 @@ window.addEvent('domready', function() {
                                                              'background-image': 'url(templates/default/images/closed.png)'}); },
 
     });
-
-    editbox = new LightFace({title: 'Edit Reflective Summary',
-                             draggable: true,
-                             buttons: [
-                                      { title: 'Save', event: function() { $('summaryform***id***').submit(); }, color: 'blue' },
-                                      { title: 'Cancel', event: function() { this.close(); } }
-                                      ],
-                             content: '<p class="left">Enter your reflective summary here. Note that newlines will be preserved, but any HTML formatting will be stripped.</p><form id="summaryform***id***" action="index.cgi" method="post"><input type="hidden" name="block" value="summary" /><input type="hidden" name="sortid" value="***id***" /><textarea id="summarytext***id***" name="summarytext***id***" rows="10" cols="76"></textarea></form>'
-    });
-
-    $('summarytext***id***').value = $('summarydata***id***').innerHTML;
 });
