4570106a269cd8b87ded19b29f609f2ab9be4716
Chris <chris@starforge.co.uk>
2012-04-16 13:21:36 +0100

    Moved doxyfile out into supportfiles.

--- makedocs.sh
+++ makedocs.sh
@@ -3,4 +3,4 @@
 # Get the latest tag annotation out of git.
 VERS=`git tag -n1 | tail -n1 | perl -e '$tag = <STDIN>; $tag =~ s/^(.*?)\s\s+(.*)$/$1 - $2/; print $tag;'`
 
-(cat Doxyfile; echo "PROJECT_NUMBER = \"$VERS\"") | doxygen -
+(cat supportfiles/Doxyfile; echo "PROJECT_NUMBER = \"$VERS\"") | doxygen -
